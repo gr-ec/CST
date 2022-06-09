023e24f9b5cd2da91ffb5986ee04bb7275752100
Guilherme Rocha <gr.ec@outlook.com>
2022-06-08 21:55:34 -0300

    Adicionar arquivos de projeto.

---
+++ CST/Program.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CST
+{
+  /// <summary>
+  /// SAMPLE INPUT:
+  /// 12/11/2020
+  /// 4
+  /// 2000000 Private 12/29/2025
+  /// 400000 Public 07/01/2020
+  /// 5000000 Public 01/02/2024
+  /// 3000000 Public 10/26/2023
+  /// 
+  /// SAMPLE OUTPUT:
+  /// HIGHRISK
+  /// EXPIRED
+  /// MEDIUMRISK
+  /// MEDIUMRISK
+  /// </summary>
+  class Program
+  {
+    /// <summary>
+    /// Stores category discovery functions <see cref="Trade"/>.
+    /// </summary>
+    delegate void TradeCategory(DateTime refDate, Trade trade);
+    static TradeCategory CategoryDiscoveryDelegates;
+
+    /// <summary>
+    /// Get input lines from user
+    /// </summary>
+    private static List<string> GetInputLines()
+    {
+      List<string> lines = new List<string>();
+
+      Console.WriteLine("Paste your input (then press enter): ");
+
+      string line;
+      while (!string.IsNullOrEmpty(line = Console.ReadLine()))
+      {
+        lines.Add(line);
+      }
+
+      return lines;
+    }
+
+    /// <summary>
+    /// Parse trade lines. Returns trades and possible errors (ignore trades if errors).
+    /// </summary>
+    private static Tuple<IList<Trade>, IList<string>> ParseTrades(List<string> lines)
+    {
+      var trades = new List<Trade>();
+      var errors = new List<string>();
+
+      if (lines.Count() < 3)
+      {
+        errors.Add("input must have at least three lines");
+      }
+      if (!DateTime.TryParseExact(lines[0].Trim(), "MM/dd/yyyy", new CultureInfo("en-US"), DateTimeStyles.None, out DateTime refDate))
+      {
+        errors.Add("Invalid format for reference date! (use format mm/dd/yyyy)");
+      }
+      if (!int.TryParse(lines[1].Trim(), out int tradesNo) || tradesNo < 1)
+      {
+        errors.Add("Invalid number of trades! (use integer numbers above 0)");
+      }
+
+      foreach (var l in lines.GetRange(2, tradesNo))
+      {
+        var tradeDataParts = l.Trim().Split(' ');
+        if (tradeDataParts.Count() == 3)
+        {
+          if (double.TryParse(tradeDataParts[0], out double tradeValue))
+          {
+            if (Trade.ClientSectorType(tradeDataParts[1].Trim()) != Trade.ClientSectorTypes.unknown)
+            {
+              if (DateTime.TryParseExact(tradeDataParts[2], "MM/dd/yyyy", new CultureInfo("en-US"), DateTimeStyles.None, out DateTime nextPaymentDate))
+              {
+                var trade = new Trade(tradeValue, tradeDataParts[1].Trim(), nextPaymentDate);
+                CategoryDiscoveryDelegates(refDate, trade);
+                trades.Add(trade);
+              }
+              else
+              {
+                errors.Add("Invalid next payment date");
+              }
+            }
+            else
+            {
+              errors.Add("Invalid client sector");
+            }
+          }
+          else
+          {
+            errors.Add("First field must be a double value");
+          }
+        }
+        else
+        {
+          errors.Add("You must inform three fields for trade fields");
+        }
+      }
+
+      return new Tuple<IList<Trade>, IList<string>>(trades, errors);
+    }
+
+    
+    static void Main(string[] args)
+    {
+      //subscribe all trade functions in order of precedence
+      CategoryDiscoveryDelegates += Trade.TradeExpired;
+      CategoryDiscoveryDelegates += Trade.TradeHighRisk;
+      CategoryDiscoveryDelegates += Trade.TradeMediumRisk;
+
+      //unsubscribe trade functions on demand
+
+      var tradeData = ParseTrades(GetInputLines());
+
+      if (tradeData.Item2.Any())
+      {
+        foreach(var e in tradeData.Item2.Distinct().ToList())
+        {
+          Console.WriteLine(e);
+        }
+      }
+      else
+      {
+        foreach (var t in tradeData.Item1)
+        {
+          t.PrintCategory();
+        }
+      }
+
+      Console.WriteLine();
+      Console.WriteLine("Press any key to exit");
+      Console.ReadLine();
+    }
+  }
+}
---
+++ CST/Trade.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CST
+{
+  class Trade : ITrade
+  {
+    public Trade(double value, string clientSector, DateTime nextPaymentDate)
+    {
+      Value = value;
+      ClientSector = clientSector;
+      NextPaymentDate = nextPaymentDate;
+    }
+
+    /// <summary>
+    /// Enum created to facilitate comparisons.
+    /// </summary>
+    public enum ClientSectorTypes
+    {
+      privateSector,
+      publicSector,
+      unknown
+    }
+
+    public double Value { get; private set; }
+    public string ClientSector { get; private set; }
+    public DateTime NextPaymentDate { get; private set; }
+    public string Category { get; private set; }
+
+    /// <summary>
+    /// Category console print if category is not null or empty.
+    /// </summary>
+    public void PrintCategory()
+    {
+      Console.WriteLine(string.IsNullOrEmpty(this.Category) ? "Unknown category" : this.Category);
+    }
+
+    /// <summary>
+    /// Set trade's category.
+    /// </summary>
+    private void SetCategory(string category)
+    {
+      if (string.IsNullOrEmpty(this.Category))
+      {
+        this.Category = category;
+      }
+    }
+
+    /// <summary>
+    /// Returns client sector type of the received sector.
+    /// </summary>
+    public static ClientSectorTypes ClientSectorType(string sector)
+    {
+      return
+        sector == "Private" ?
+          ClientSectorTypes.privateSector :
+          sector == "Public" ?
+            ClientSectorTypes.publicSector :
+            ClientSectorTypes.unknown;
+    }
+
+    /// <summary>
+    /// Returns client sector type of this trade.
+    /// </summary>
+    public ClientSectorTypes MyClientSectorType()
+    {
+      return ClientSectorType(this.ClientSector);
+    }
+
+    #region category discovery delegates
+    public static void TradeExpired(DateTime refDate, Trade trade)
+    {
+      if (refDate.Subtract(trade.NextPaymentDate).TotalDays > 30)
+      {
+        trade.SetCategory("EXPIRED");
+      }
+    }
+    public static void TradeHighRisk(DateTime refDate, Trade trade)
+    {
+      if (trade.Value > 1000000 && trade.MyClientSectorType() == ClientSectorTypes.privateSector)
+      {
+        trade.SetCategory("HIGHRISK");
+      }
+    }
+    public static void TradeMediumRisk(DateTime refDate, Trade trade)
+    {
+      if (trade.Value > 1000000 && trade.MyClientSectorType() == ClientSectorTypes.publicSector)
+      {
+        trade.SetCategory("MEDIUMRISK");
+      }
+    }
+    #endregion
+  }
+}
